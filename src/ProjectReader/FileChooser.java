76614c42a3f894f6c411147f547ef3b456668bc8
Kiowa Daly <kiowa.daly@ucdconnect.ie>
2019-02-11 10:31:12 +0000

    file Choose -code is from javaDocs for "JFileChooser"

--- src/ProjectReader/FileChooser.java
+++ src/ProjectReader/FileChooser.java
@@ -1,16 +1,14 @@
 package ProjectReader;
-
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
-import java.io.IOException;
 
 public class FileChooser {
    //open file explorer
-    Process myFile;
+
     public void selectFile() {
         JFrame myComponent = new JFrame();
         JFileChooser chooser = new JFileChooser();
-        FileNameExtensionFilter filter = new FileNameExtensionFilter("JPG & GIF Images","jpg","gif");
+        FileNameExtensionFilter filter = new FileNameExtensionFilter(".Class files","class");
         chooser.setFileFilter(filter);
         int returnVal = chooser.showOpenDialog(myComponent);
         if(returnVal == JFileChooser.APPROVE_OPTION){
