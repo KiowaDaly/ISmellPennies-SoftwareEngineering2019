5aa40c5d26866d4ca192b7250acb482cf35f5be8
Rebecca Lobo <rebecca.lobo@ucdconnect.ie>
2019-03-06 21:17:35 +0000

    using Fields and Method types to obtain variables and method types from Classes

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 import BloatedCodeCheckers.ClassBloatedCodeChecker;
-
 import BloatedCodeCheckers.BloatedCodeChecker;
 import BloatedCodeCheckers.MethodBloatedCodeChecker;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 
 public class Main {
@@ -15,5 +16,25 @@ public class Main {
         System.out.println(modularCheck.getClassName() +" has "+ modularCheck.getNumberOfMethods() +" Methods-including its subclasses");
         System.out.println(methodCheck.methodHasTooManyParameters());
 
+        //06/03/2019 - obtaining methods/ variable types from classes
+        //example using BloatedCodeChecker
+
+        Field df[] = MethodBloatedCodeChecker.class.getDeclaredFields();
+        Field f[] = MethodBloatedCodeChecker.class.getFields();
+
+        //Protected, private, public
+        System.out.println("\nDeclared fields: ");
+        for(Field f1: df){
+            System.out.println(f1.toString());
+        }
+        //Just public
+        System.out.println("\nFields: ");
+        for(Field f2: f) System.out.println("\n" + f2.toString());
+
+        //Class methods
+        System.out.println("\nClass Methods: ");
+        for(Method m: MethodBloatedCodeChecker.class.getMethods()) System.out.println(m.toString());
+
+        //this whole section of code can be snipped and slotted into proper classes once created - not to stay in class.Main
     }
 }
