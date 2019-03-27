8a55c0f78e3da0bf181535511f25d4451e23aede
Rebecca Lobo <rebecca.lobo@ucdconnect.ie>
2019-03-27 21:31:18 +0000

    work on the Visitor - method Collector

---
+++ src/JavaFileVisitors/MethodCollector.java
@@ -0,0 +1,16 @@
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.visitor.VoidVisitor;
+import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+import parser.JavaFileParser;
+import com.github.javaparser.StaticJavaParser;
+
+import java.util.List;
+
+public class MethodCollector extends VoidVisitorAdapter<List<String>> {
+    public void visit(MethodDeclaration md, List<String> collector) {
+        super.visit(md, collector);
+        collector.add(md.getNameAsString());
+    }
+}
