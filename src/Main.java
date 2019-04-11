9344f82bf250bffb2bd96875efac5f27fd7e4c54
Kiowa Daly <kiowa.daly@ucdconnect.ie>
2019-04-11 20:47:43 +0100

    started a javaFX sceneBuilder to aid with the ui creation

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,20 @@
-
-import Project_FileAnalyser.FileChooser;
-
-import java.io.File;
-
-
-public class Main {
-    private static final String FILE_PATH = "src/test.java";
-
-    public static void main(String[] argc) throws Exception {
-
-        FileChooser FileExplorer = new FileChooser();
-        File ourProject = FileExplorer.selectFolder();
-        System.out.println("----------> SMELL CHECK RESULTS <----------");
-        FileExplorer.loopFolders(ourProject);
-
-    }
-
-
-}
+//
+//import Project_FileAnalyser.FileChooser;
+//
+//import java.io.File;
+//
+//
+//public class Main {
+//    private static final String FILE_PATH = "src/test.java";
+//
+//    public static void main(String[] argc) throws Exception {
+//
+//        FileChooser FileExplorer = new FileChooser();
+//        File ourProject = FileExplorer.selectFolder();
+//        System.out.println("----------> SMELL CHECK RESULTS <----------");
+//        FileExplorer.loopFolders(ourProject);
+//
+//    }
+//
+//
+//}
---
+++ src/UI/JavaFXStage.java
@@ -0,0 +1,74 @@
+package UI;
+
+import javafx.application.Application;
+import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.TransferMode;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class JavaFXStage extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        Label label = new Label("Drag project here!");
+        Label dropped = new Label("");
+        VBox dragTarget = new VBox();
+        dragTarget.getChildren().addAll(label,dropped);
+        dragTarget.setOnDragOver(new EventHandler<DragEvent>() {
+
+            @Override
+            public void handle(DragEvent event) {
+                if (event.getGestureSource() != dragTarget
+                        && event.getDragboard().hasFiles()) {
+                    /* allow for both copying and moving, whatever user chooses */
+                    event.acceptTransferModes(TransferMode.COPY_OR_MOVE);
+                }
+                event.consume();
+            }
+        });
+
+        dragTarget.setOnDragDropped(new EventHandler<DragEvent>() {
+
+            @Override
+            public void handle(DragEvent event) {
+                Dragboard db = event.getDragboard();
+                boolean success = false;
+                if (db.hasFiles()) {
+                    dropped.setText(db.getFiles().toString());
+                    success = true;
+                }
+                /* let the source know whether the string was successfully
+                 * transferred and used */
+                event.setDropCompleted(success);
+
+                event.consume();
+            }
+        });
+
+
+        StackPane root = new StackPane();
+        root.getChildren().add(dragTarget);
+
+        Scene scene = new Scene(root, 1000, 800);
+
+        primaryStage.setTitle("Drag Test");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    }
+
