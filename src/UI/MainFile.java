2d110f051bdf35e760941da5f01b53cdbecaa611
Kiowa Daly <kiowa.daly@ucdconnect.ie>
2019-04-13 16:08:01 +0100

    renamed fileChooser to fileHandler, changed select file from using Swing to using javafx - cleaner feel

--- src/UI/MainFile.java
+++ src/UI/MainFile.java
@@ -1,26 +1,12 @@
 package UI;
 
 import javafx.application.Application;
-import javafx.collections.ObservableList;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.input.DragEvent;
-import javafx.scene.input.Dragboard;
-import javafx.scene.input.TransferMode;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
-
 import java.io.IOException;
-import java.net.URL;
+import java.util.Objects;
 
 public class MainFile extends Application {
 
@@ -30,8 +16,8 @@ public class MainFile extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        Parent root =  FXMLLoader.load( this.getClass().getClassLoader()
-                .getResource("src/UI/baseScene.fxml"));
+        Parent root =  FXMLLoader.load(Objects.requireNonNull(this.getClass().getClassLoader()
+                .getResource("src/UI/baseScene.fxml")));
 
 
         // Load the FXML document
