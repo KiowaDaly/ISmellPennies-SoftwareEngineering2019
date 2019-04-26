e8f64d4e17eb631252bea6c14196dff63ebaa5f3
Joiedel Agustin <Joiedel@hotmail.com>
2019-04-26 18:12:22 +0100

    dummy class
    just testing commit

--- src/Lazies_Freeloader_walkingdead/WalkingDeadChecks.java
+++ src/Lazies_Freeloader_walkingdead/WalkingDeadChecks.java
@@ -3,8 +3,12 @@ import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.body.FieldDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.body.VariableDeclarator;
+import com.github.javaparser.ast.expr.MethodCallExpr;
+
 import java.util.ArrayList;
 import java.util.List;
+
+import utility_classes.ThreatLevel;
 
 public class WalkingDeadChecks {
 
@@ -23,4 +27,26 @@ public class WalkingDeadChecks {
         return nonDataMethods.size() == 0;
 
     }
+
+    //check how many times its called in the javaparser
+    public boolean isDeadCode(ClassOrInterfaceDeclaration c1){
+        //dead methods
+        //if mthod calls is 0, print method name and its count and make it a threat
+        List<MethodCallExpr> methodCalls = new ArrayList<>();
+        c1.isCallableDeclaration();
+        c1.getMethods();
+
+        return false;
+    }
+
+        //to do
+    public boolean isLazyCode(ClassOrInterfaceDeclaration c1){
+        return false;
+    }
+
+    public boolean isDuplicateCode(ClassOrInterfaceDeclaration c1){
+        return false;
+    }
+
+
 }
