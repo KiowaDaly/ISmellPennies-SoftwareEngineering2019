07dbc0bc7e47a89c6493a4c5728a5f4256a8d9e4
octalcat <aleksandar.dimitrov@ucdconnect.ie>
2019-04-26 13:21:06 +0100

    temp dummy class

---
+++ src/GodComplexes/GodClassCheck.java
@@ -0,0 +1,15 @@
+package GodComplexes;
+
+import com.github.javaparser.*;
+import utility_classes.ThreatLevel;
+
+// Analyse: coupling, cohesion, complexity and size.
+
+public class GodClassCheck
+{
+  // dummy
+  private ThreatLevel checkGodClass(ClassOrInterfaceDeclaration clin)
+  {
+    return ThreatLevel.HIGH;
+  }
+}
